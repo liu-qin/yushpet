1c362ddc3aba684d8988887ba4051b676d7269aa
k2wf8848 <k2wf@qq.com>
2018-10-10 12:28:43 +0800

    127.0.0.1

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,11 +1,9 @@
 <?php
 //数据库连接信息
-$cfg_dbhost = '218.244.149.144';
+$cfg_dbhost = '127.0.0.1';
 $cfg_dbname = 'yashi';
 $cfg_dbuser = 'root';
 $cfg_dbpwd = '';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
-
-
 ?>
