ccaaf39b965921b0ff4df3b5039ebd76682ba948
k2wf8848 <k2wf@qq.com>
2018-10-12 13:40:10 +0800

    修改原因

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,11 +1,9 @@
 <?php
 //数据库连接信息
-$cfg_dbhost = 'localhost';
+$cfg_dbhost = '218.244.149.144';
 $cfg_dbname = 'yashi';
-$cfg_dbuser = 'root';
-$cfg_dbpwd = 'root';
+$cfg_dbuser = 'yashi';
+$cfg_dbpwd = 'yashi123';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
-
-
 ?>
