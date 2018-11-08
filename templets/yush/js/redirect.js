e2f30fa36a5be13c585d3f2645a76b5cacdbd441
k2wf8848 <k2wf@qq.com>
2018-11-08 14:37:54 +0800

    调整JS

--- templets/yush/js/redirect.js
+++ templets/yush/js/redirect.js
@@ -1,11 +1,14 @@
-var userAgentInfo = navigator.userAgent.toLowerCase();
-var Agents = ["android", "iphone",
+<script>
+　　//判断是否手机端访问
+    var userAgentInfo = navigator.userAgent.toLowerCase();
+    var Agents = ["android", "iphone",
                 "symbianos", "windows phone",
                 "ipad", "ipod"];
-var ly=document.referrer;  //返回导航到当前网页的超链接所在网页的URL
+    var ly=document.referrer;  //返回导航到当前网页的超链接所在网页的URL
 
-for (var v = 0; v < Agents.length; v++) {
-    if (userAgentInfo.indexOf(Agents[v]) >= 0&&(ly==""||ly==null)) {
-         this.location.href='http://m.yushpet.com/index.html';  //wap端地址
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) >= 0&&(ly==""||ly==null)) {
+            this.location.href='http://m.yushpet.com/index.html';  //wap端地址
+        }
     }
-}
+</script>
