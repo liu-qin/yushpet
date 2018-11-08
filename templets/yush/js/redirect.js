31f6ee1ecec5e3ab8a0779b0400ddda62b188ea8
k2wf8848 <k2wf@qq.com>
2018-11-08 19:33:32 +0800

    改JS

--- templets/yush/js/redirect.js
+++ templets/yush/js/redirect.js
@@ -1,13 +1,2 @@
-
-　　//判断是否手机端访问z
-    var userAgentInfo = navigator.userAgent.toLowerCase();
-    var Agents = ["android", "iphone",
-                "symbianos", "windows phone",
-                "ipad", "ipod"];
-    var ly=document.referrer;  //返回导航到当前网页的超链接所在网页的URL
-
-    for (var v = 0; v < Agents.length; v++) {
-        if (userAgentInfo.indexOf(Agents[v]) >= 0&&(ly==""||ly==null)) {
-            this.location.href='http://m.yushpet.com/index.html';  //wap端地址
-        }
-    }
+if(navigator.platform.indexOf('Win32')!=-1){}
+else{window.location.href="{dede:global.wapurl/}"+window.location.pathname;}
